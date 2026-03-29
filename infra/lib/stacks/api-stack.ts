b43e23bc0b0fb2d72e51ae81b98cca09bcddd651
Roddy Tomlin <roddytdog@yahoo.com>
2026-03-29 11:15:40 -0500

    Grant API Lambda DynamoDB table access and explicit table mappings

--- infra/lib/stacks/api-stack.ts
+++ infra/lib/stacks/api-stack.ts
@@ -1,5 +1,6 @@
 import { CfnOutput, Stack } from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { DotnetLambdaFunction } from '../constructs/lambda-function';
@@ -22,12 +23,27 @@ export class ApiStack extends Stack {
         COGNITO_CLIENT_ID: props.auth.userPoolClientId,
         OPENAI_SECRET_ARN: props.secrets.openAiSecretArn,
         TABLE_PREFIX: prefix,
-        RECIPE_IMAGES_BUCKET: props.data.recipeImagesBucketName
+        RECIPE_IMAGES_BUCKET: props.data.recipeImagesBucketName,
+        DynamoDb__PartitionKeyName: 'PK',
+        DynamoDb__SortKeyName: 'SK',
+        DynamoDb__Tables__UserProfileDocument: props.data.tableNames.Users,
+        DynamoDb__Tables__DependentProfileDocument: props.data.tableNames.Users,
+        DynamoDb__Tables__RecipeDocument: props.data.tableNames.Recipes,
+        DynamoDb__Tables__FavoriteRecipeDocument: props.data.tableNames.Favorites,
+        DynamoDb__Tables__MealPlanDocument: props.data.tableNames.MealPlans,
+        DynamoDb__Tables__GroceryListDocument: props.data.tableNames.GroceryLists,
+        DynamoDb__Tables__PantryStaplesDocument: props.data.tableNames.GroceryLists,
+        DynamoDb__Tables__ChatHistoryMessageDocument: props.data.tableNames.ChatHistory
       }
     });
 
     const recipeImagesBucket = s3.Bucket.fromBucketName(this, 'RecipeImagesBucket', props.data.recipeImagesBucketName);
     recipeImagesBucket.grantReadWrite(handler.function);
+
+    Object.values(props.data.tableNames).forEach((tableName) => {
+      const table = dynamodb.Table.fromTableName(this, `${tableName}Ref`, tableName);
+      table.grantReadWriteData(handler.function);
+    });
 
     const api = new apigateway.LambdaRestApi(this, 'RestApi', {
       restApiName: `${prefix}-api`,
